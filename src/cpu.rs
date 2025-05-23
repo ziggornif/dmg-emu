dfabf14d3a9f1d1d6ef3c87442f8dcfc95b405a6
ziggornif <matthieudrouian@gmail.com>
2025-05-23 08:19:09 +0200

    feat(cpu): add registers

---
+++ src/cpu.rs
@@ -0,0 +1,73 @@
+const FLAG_Z: u8 = 0b10000000; // Zero
+const FLAG_N: u8 = 0b01000000; // Subtraction
+const FLAG_H: u8 = 0b00100000; // Half Carry
+const FLAG_C: u8 = 0b00010000; // Carry
+
+pub struct CPU {
+    // 8bit registers
+    pub a: u8,
+    pub b: u8,
+    pub c: u8,
+    pub d: u8,
+    pub e: u8,
+    pub f: u8,
+    pub h: u8,
+    pub l: u8,
+
+    // 16bit registers
+    pub sp: u16, // stack pointer
+    pub pc: u16, // program counter
+}
+
+impl CPU {
+    pub fn new() -> Self {
+        Self {
+            a: 0,
+            b: 0,
+            c: 0,
+            d: 0,
+            e: 0,
+            f: 0,
+            h: 0,
+            l: 0,
+            sp: 0,
+            pc: 0,
+        }
+    }
+
+    pub fn af(&self) -> u16 {
+        ((self.a as u16) << 8) | (self.f as u16)
+    }
+
+    pub fn set_af(&mut self, value: u16) {
+        self.a = (value >> 8) as u8;
+        self.f = (value & 0xF0) as u8;
+    }
+
+    pub fn bc(&self) -> u16 {
+        ((self.b as u16) << 8) | (self.c as u16)
+    }
+
+    pub fn set_bc(&mut self, value: u16) {
+        self.b = (value >> 8) as u8;
+        self.c = value as u8;
+    }
+
+    pub fn de(&self) -> u16 {
+        ((self.d as u16) << 8) | (self.e as u16)
+    }
+
+    pub fn set_de(&mut self, value: u16) {
+        self.d = (value >> 8) as u8;
+        self.e = value as u8;
+    }
+
+    pub fn hl(&self) -> u16 {
+        ((self.h as u16) << 8) | (self.l as u16)
+    }
+
+    pub fn set_hl(&mut self, value: u16) {
+        self.h = (value >> 8) as u8;
+        self.l = value as u8;
+    }
+}
