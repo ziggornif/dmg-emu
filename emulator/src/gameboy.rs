779a60ebd0716034c990267ffdedebf6df3b2903
ziggornif <matthieudrouian@gmail.com>
2025-06-22 18:31:30 +0200

    feat: use joypad and timer in gb emulator file

--- emulator/src/gameboy.rs
+++ emulator/src/gameboy.rs
@@ -1,9 +1,12 @@
-use crate::{bus::Bus, cpu::CPU, error, info, print_cpu_state, print_ppu_state};
+use crate::joypad::JoypadButton;
+use crate::{bus::Bus, cpu::CPU, debug, error, print_cpu_state, print_ppu_state};
 
 #[derive(Debug, Clone)]
 pub struct Gameboy {
     pub cpu: CPU,
     pub bus: Bus,
+    pub last_pc: u16,
+    pub pc_repeat_count: u32,
 }
 
 impl Gameboy {
@@ -11,6 +14,8 @@ impl Gameboy {
         Self {
             cpu: CPU::new(),
             bus: Bus::new(),
+            last_pc: 0,
+            pc_repeat_count: 0,
         }
     }
 
@@ -37,16 +42,35 @@ impl Gameboy {
         }
     }
 
+    pub fn handle_input(&mut self, button: JoypadButton, pressed: bool) {
+        self.bus.set_joypad_input(button, pressed);
+
+        if pressed && self.bus.joypad_interrupt() {
+            let if_reg = self.bus.read_byte(0xFF0F);
+            self.bus.write_byte(0xFF0F, if_reg | 0x10);
+        }
+    }
+
     pub fn step(&mut self) -> bool {
-        if self.cpu.pc == 0x0100 {
-            info!("Back to program start - possible infinite loop");
+        if self.cpu.pc == self.last_pc {
+            self.pc_repeat_count += 1;
+            if self.pc_repeat_count > 1000 {
+                println!("\n=== INFINITE LOOP DETECTED ===");
+                println!("PC at: 0x{:04X}", self.cpu.pc);
+                return true;
+            }
+        } else {
+            self.last_pc = self.cpu.pc;
+            self.pc_repeat_count = 0;
         }
 
         if let Some(data) = self.get_serial_output() {
-            if data as char == ' ' {
-                print!("\n")
+            let ch = data as char;
+            if ch == '\n' || ch == ' ' {
+                print!("\n");
+            } else {
+                print!("{}", ch);
             }
-            print!("{}", data as char);
         }
 
         self.validate_pc();
@@ -55,20 +79,75 @@ impl Gameboy {
         self.cpu.pc = self.cpu.pc.wrapping_add(1);
         let cycles = self.cpu.execute_instruction(opcode, &mut self.bus);
 
+        let timer_interrupt = self.bus.timer_step(cycles);
+        if timer_interrupt {
+            let if_reg = self.bus.read_byte(0xFF0F);
+            self.bus.write_byte(0xFF0F, if_reg | 0x04);
+        }
+
         let vblank_interrupt = self.bus.ppu_step(cycles);
 
-        if vblank_interrupt && self.cpu.interrupts_enabled() {
-            self.cpu.wake_from_halt();
-            self.handle_vblank_interrupt();
-        }
+        self.handle_interrupts(vblank_interrupt, timer_interrupt);
 
         vblank_interrupt
     }
 
-    fn handle_vblank_interrupt(&mut self) {
-        self.cpu.stack_push(&mut self.bus, self.cpu.pc);
-        self.cpu.pc = 0x0040;
+    fn handle_interrupts(&mut self, vblank: bool, timer: bool) {
+        if !self.cpu.interrupts_enabled() {
+            return;
+        }
+
+        let mut interrupt_triggered = false;
+
+        // VBlank interrupt
+        if vblank {
+            let ie_reg = self.bus.read_byte(0xFFFF);
+            if (ie_reg & 0x01) != 0 {
+                debug!("🔥 Handling VBlank interrupt");
+                self.handle_interrupt(0x40, 0x01);
+                interrupt_triggered = true;
+            }
+        }
+
+        // Timer interrupt
+        if timer && !interrupt_triggered {
+            let ie_reg = self.bus.read_byte(0xFFFF);
+            if (ie_reg & 0x04) != 0 {
+                debug!("🔥 Handling Timer interrupt");
+                self.handle_interrupt(0x50, 0x04);
+                interrupt_triggered = true;
+            }
+        }
+
+        // Others
+        if !interrupt_triggered {
+            let if_reg = self.bus.read_byte(0xFF0F);
+            let ie_reg = self.bus.read_byte(0xFFFF);
+            let pending = if_reg & ie_reg;
+
+            if pending & 0x02 != 0 {
+                // LCD STAT interrupt
+                debug!("🔥 Handling LCD STAT interrupt");
+                self.handle_interrupt(0x48, 0x02);
+            } else if pending & 0x08 != 0 {
+                // Serial interrupt
+                debug!("🔥 Handling Serial interrupt");
+                self.handle_interrupt(0x58, 0x08);
+            } else if pending & 0x10 != 0 {
+                // Joypad interrupt
+                debug!("🔥 Handling Joypad interrupt");
+                self.handle_interrupt(0x60, 0x10);
+            }
+        }
+    }
+
+    fn handle_interrupt(&mut self, vector: u16, flag_bit: u8) {
         self.cpu.disable_interrupts();
+        self.cpu.stack_push(&mut self.bus, self.cpu.pc);
+        self.cpu.pc = vector;
+
+        let if_reg = self.bus.read_byte(0xFF0F);
+        self.bus.write_byte(0xFF0F, if_reg & !flag_bit);
     }
 
     pub fn run_frame(&mut self) -> u32 {
